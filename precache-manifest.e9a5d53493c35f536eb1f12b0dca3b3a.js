d798c115d4368b34c94c863693a445260345bb53
kate <bkateryna27@icloud.com>
2019-12-03 13:31:03 -0500

    Updates

---
+++ precache-manifest.e9a5d53493c35f536eb1f12b0dca3b3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6cdbdb796a43217ed2af6c5a214893d",
+    "url": "/friends-game-react/index.html"
+  },
+  {
+    "revision": "75cd2f868be78636b999",
+    "url": "/friends-game-react/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "93f88c2b4519c5681f11",
+    "url": "/friends-game-react/static/js/2.6bbc9386.chunk.js"
+  },
+  {
+    "revision": "75cd2f868be78636b999",
+    "url": "/friends-game-react/static/js/main.4c93f5f2.chunk.js"
+  },
+  {
+    "revision": "a105f70fd13d1c16dbca",
+    "url": "/friends-game-react/static/js/runtime-main.f16e3e25.js"
+  }
+]);
